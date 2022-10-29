35ebeeabf2d7f46f4eb050669b30791de8db86a6
A <movie3342@gmail.com>
2022-10-29 19:59:37 +0900

    ContextAPI 구현

---
+++ src/lib/state/ContextAPI.tsx
@@ -0,0 +1,92 @@
+import { createContext, ReactNode, useState } from "react";
+
+interface RepoListProps {
+  number: number;
+  id: number;
+  title: string;
+  user: {
+    id: number;
+    login: string;
+    avatar_url: string;
+    url: string;
+  };
+  created_at: string;
+  comments: number;
+}
+
+interface RepoDetailProps extends RepoListProps {
+  body: string;
+}
+
+interface IssueListContextAPIProps {
+  issues?: RepoListProps[];
+  setIssues: React.Dispatch<React.SetStateAction<RepoListProps[]>>;
+}
+
+interface IssueDetailContextAPIProps {
+  detail?: RepoDetailProps;
+  setDetail: React.Dispatch<React.SetStateAction<RepoDetailProps>>;
+}
+export const IssueListContextAPI = createContext<IssueListContextAPIProps>(
+  null!
+);
+export const IssueDetailContextAPI = createContext<IssueDetailContextAPIProps>(
+  null!
+);
+interface ContextAPIProps {
+  children: ReactNode;
+}
+
+const issueDefaultValue = [
+  {
+    number: 0,
+    id: 0,
+    title: "",
+    user: {
+      id: 0,
+      login: "",
+      avatar_url: "",
+      url: ""
+    },
+    created_at: "",
+    comments: 0
+  }
+];
+
+const detailDefaultValue = {
+  number: 0,
+  id: 0,
+  title: "",
+  user: {
+    id: 0,
+    login: "",
+    avatar_url: "",
+    url: ""
+  },
+  created_at: "",
+  comments: 0,
+  body: ""
+};
+
+export const ContextAPIProvider = ({ children }: ContextAPIProps) => {
+  const [issues, setIssues] = useState<RepoListProps[]>(issueDefaultValue);
+  const [detail, setDetail] = useState<RepoDetailProps>(detailDefaultValue);
+
+  const issueValue = {
+    issues,
+    setIssues
+  };
+
+  const detailValue = {
+    detail,
+    setDetail
+  };
+
+  return (
+    <IssueListContextAPI.Provider value={issueValue}>
+      <IssueDetailContextAPI.Provider value={detailValue}>
+        {children}
+      </IssueDetailContextAPI.Provider>
+    </IssueListContextAPI.Provider>
+  );
+};
