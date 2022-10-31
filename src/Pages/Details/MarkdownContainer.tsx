4a409a7f4754fbfe906403c19f6fd9e93de6883a
A <movie3342@gmail.com>
2022-10-31 14:35:53 +0900

    rmarkGfm(깃허브 스타일 마크다운 플러그인)추가 및 스타일

--- src/Pages/Details/MarkdownContainer.tsx
+++ src/Pages/Details/MarkdownContainer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import styled from "styled-components";
 import CodeBlock from "./CodeBlock";
-import { BlockQuote, Table, ContainsTaskList, A } from "./styles";
+import {
+  BlockQuote,
+  Table,
+  ContainsTaskList,
+  A,
+  MarkdownStyle
+} from "./styles";
 
 interface IMarkdownContainerProps {
   markdown: string;
@@ -9,37 +17,44 @@ interface IMarkdownContainerProps {
 
 const MarkdownContainer = ({ markdown }: IMarkdownContainerProps) => {
   return (
-    <Markdown
-      components={{
-        code: (props) => <CodeBlock {...props} />,
-        tr({ children }) {
-          return <tr>{children}</tr>;
-        },
-        blockquote({ children }) {
-          return <BlockQuote>{children}</BlockQuote>;
-        },
-        table({ children }) {
-          return <Table>{children}</Table>;
-        },
-        ul({ children, className }) {
-          return className?.includes("contains-task-list") ? (
-            <ContainsTaskList>{children}</ContainsTaskList>
-          ) : (
-            <ul className={className}>{children}</ul>
-          );
-        },
-        a({ children, href }) {
-          return (
-            <A href={href} rel="noreferrer" target="_blank">
-              {children}
-            </A>
-          );
-        }
-      }}
-    >
-      {markdown}
-    </Markdown>
+    <MarkdownWrapper>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code: (props) => <CodeBlock {...props} />,
+          tr({ children }) {
+            return <tr>{children}</tr>;
+          },
+          blockquote({ children }) {
+            return <BlockQuote>{children}</BlockQuote>;
+          },
+          table({ children }) {
+            return <Table>{children}</Table>;
+          },
+          ul({ children, className }) {
+            return className?.includes("contains-task-list") ? (
+              <ContainsTaskList>{children}</ContainsTaskList>
+            ) : (
+              <ul className={className}>{children}</ul>
+            );
+          },
+          a({ children, href }) {
+            return (
+              <A href={href} rel="noreferrer" target="_blank">
+                {children}
+              </A>
+            );
+          }
+        }}
+      >
+        {markdown}
+      </Markdown>
+    </MarkdownWrapper>
   );
 };
 
 export default MarkdownContainer;
+
+const MarkdownWrapper = styled.div`
+  ${MarkdownStyle}
+`;
